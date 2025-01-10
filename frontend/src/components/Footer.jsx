efdc5a84bedfc65b4aaa78ea67ee3928f3f9c437
oclark3 <oclark73737@yahoo.com>
2025-01-09 20:32:25 -0600

    update github link

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => {
                 </a>
                 . The source code is available on{" "}
                 <a 
-                    href="https://github.com/oclark3/netflix-clone"
+                    href="https://github.com/oclark3/mern-netflix-clone"
                     target='_blank'
                     rel='noreferrer'
                     className='font-medium underline underline-offset-4'
